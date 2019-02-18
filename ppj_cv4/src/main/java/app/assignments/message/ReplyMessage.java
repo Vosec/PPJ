cc9df8de886f71d511a9c114fcba675e313de5ae
Lukas_Vosecky <lukas.vosecky@tul.cz>
2019-02-18 18:19:05 +0100

    r

---
+++ ppj_cv4/src/main/java/app/assignments/message/ReplyMessage.java
@@ -0,0 +1,35 @@
+package app.assignments.message;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("pingMessageReply")
+public class ReplyMessage implements Message {
+
+    @Autowired
+    @Qualifier("pingMessage")
+    private Message original;
+
+    @Value("ahoj")
+    private String reply;
+
+    /**
+    public ReplyMessage(Message original, String reply) {
+        this.original = original;
+        this.reply = reply;
+    }
+    */
+    public String getSender() {
+        return original.getRecipient();
+    }
+
+    public String getRecipient() {
+        return original.getSender();
+    }
+
+    public String getText() {
+        return "ORIGINAL:\n" + original.getText() + "\nREPLY:\n" + reply;
+    }
+}
